eafca26883d70e9a277ab57ae5deaf28d5df711c
hibiyaQAQ <2533613700@qq.com>
2026-05-19 19:04:53 +0800

    Add files via upload

---
+++ api/v1/models.ts
@@ -0,0 +1,9 @@
+import { handleRequest } from "../../edge/wald";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function handler(request: Request): Promise<Response> {
+  return handleRequest(request);
+}
